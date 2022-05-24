5594c6de7c148e463829dd70692a84e5ee12857d
Virendra kumar dwivedi <101566111+Virendrakumardwivedi@users.noreply.github.com>
2022-05-25 00:31:13 +0530

    data

---
+++ script/deshboard.js
@@ -0,0 +1,56 @@
+append();
+
+function append(){
+    console.log("yes")
+    let data=JSON.parse(localStorage.getItem("students")) || [];
+    let container = document.getElementById("container")
+     
+    data.forEach(function(el, index){
+        let div= document.createElement("div")
+        div.setAttribute("id","fundiv")
+      // console.log(el) 
+       let img=document.createElement("img")
+       img.src = el.image;
+       img.setAttribute("id","imgp")
+
+       let na = document.createElement("h3");
+       na.innerText=`Name-: ${el.name}`;
+
+       let batch = document.createElement("h3")
+       batch.innerText=`Batch-: ${el.batch}`;
+
+       let course=document.createElement("h4")
+       course.innerText=`Course-: ${el.course}`;
+
+       let unit = document.createElement("h4")
+       unit.innerText=`unit-: ${el.unit}`;
+
+
+       let btn = document.createElement("button");
+       btn.innerText="REMOVE";
+       btn.setAttribute("id","remov")
+       btn.addEventListener("click",function(){
+           remove(index);
+       });
+       div.append(img,na, batch,course,unit,btn);
+       container.append(div);
+    });  
+}
+//append();
+function remove(index){
+    let data=JSON.parse(localStorage.getItem("students")) || [];
+
+let newData = data.filter(function(el , i){
+    if(i==index){
+        let trash = JSON.parse(localStorage.getItem("trash"))||[];
+    trash.push(el);
+    localStorage.setItem("trash",JSON.stringify(trash));
+    }
+    else{
+        return i!==index;
+    }
+   
+})
+localStorage.setItem("students",JSON.stringify(newData))
+append();
+}
---
+++ script/index.js
@@ -0,0 +1,44 @@
+
+function Student(n,c,u,i,b){
+this.name=n;
+this.course=c;
+this.unit=u;
+this.image=i;
+this.batch=`Ft-Web${b}`;
+}
+function storeData(e){
+    e.preventDefault();
+  let from=document.getElementById("student_data");
+  let name=from.name.value;
+  let course = from.course.value;
+  let unit = from.unit.value;
+  let image = from.image.value;
+  let batch = from.batch.value;
+  //console.log(name,course,unit,image,batch)
+  let s1=new Student(name,course,unit,image,batch)
+  let data=JSON.parse(localStorage.getItem("students")) || [];
+  data.push(s1)
+  localStorage.setItem("students",JSON.stringify(data));
+  console.log(s1)
+
+}
+function calculate(){
+    let data=JSON.parse(localStorage.getItem("students")) || [];
+    let obj={}
+    for(let i=0; i<data.length; i++){
+        if(obj[data[i].batch]==undefined){
+            obj[data[i].batch]=1
+        }
+        else{
+            obj[data[i].batch] +=1
+        }
+    }
+    let top = document.getElementById("navbar");
+    for(var i in obj){
+        let p = document.createElement("p")
+        p.append(i,"--",obj[i])
+        top.append(p)
+    }
+    console.log(obj)
+}
+calculate();
